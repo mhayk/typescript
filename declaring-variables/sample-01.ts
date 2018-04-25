8015270abcfb72f0ae87bc894449392ee8e58a9d
Mhayk Whandson <hi@mhayk.com>
2018-04-25 22:16:13 +0100

    very simple example about let and var in typescript.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ declaring-variables/sample-01.ts
@@ -0,0 +1,9 @@
+// var number = 1;
+// let count = 2;
+function _doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+doSomething();
---
+++ declaring-variables/sample-02.ts
@@ -0,0 +1,10 @@
+// var number = 1;
+// let count = 2;
+// compile it works but the editor highlight the error...
+function doSomething() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+doSomething();
